eb9df403f444b3e050e53356226623144ba1f989
Ilya Poltawezc <ilya.poltawezc@gmail.com>
2025-12-17 23:41:47 +0300

    sader end, fix errors

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,72 +1,48 @@
 import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons'
-import { Layout, Card, Statistic, Typography, List, Spin } from 'antd';
-import { useEffect, useState } from 'react';
+import { Layout, Card, Statistic, Typography, List, Tag } from 'antd';
+import { capitalize } from '../../utils'
+import { useContext } from 'react';
+import CryptoContext from '../../context/crypto-context';
 
 const siderStyle = {
   padding: '1rem',
 }
 
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.'
-]
-
 export default function AppSider() {
-    const [loading, setLoading] = useState(false)
-    const [crypto, setCrypto] = useState([])
-    const [assets, setAssets] = useState([])
-
-    useEffect(() => {
-        async function preload() {
-            setLoading(true)
-            const { result } = await fakeFetchCrypto()
-            const assets = await fetchAssets()
-
-            setAssets(assets)
-            setCrypto(result)
-            setLoading(false)
-        }
-        preload()
-    }, [])
-
-    //if (loading) {
-       // return <Spin fullscreen />
-   // }
+    const {assets} = useContext(CryptoContext)
 
     return (
         <Layout.Sider width="25%" style={siderStyle}>
-            <Card style={ { marginBottom: '1rem'}}>
+            {assets.map(asset => (
+                <Card key={asset.id} style={ { marginBottom: '1rem'}}>
                 <Statistic
-                  title="Active"
-                  value={11.28}
+                  title={capitalize(asset.id)}
+                  value={asset.totalAmount}
                   precision={2}
-                  styles={{ content: { color: '#3f8600' } }}
-                  prefix={<ArrowUpOutlined />}
-                  suffix="%"
+                  styles={{ content: { color: asset.grow ? '#3f8600' : '#cf1322' } }}
+                  prefix={asset.grow ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+                  suffix="$"
                  />
                  <List 
                  size='small'
-                 dataSource={data}
+                 dataSource={[
+                    {title: 'Total Profit', value: asset.totalProfit, withTag: true,},
+                    {title: 'Asset Amount', value: asset.amount, isPlain: true},
+                    //{title: 'Difference', value: asset.growPercent},
+                 ]}
                  renderItem={(item) => (
                 <List.Item>
-                    <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                    <span>{item.title}</span>
+                    <span>
+                    {item.withTag && <Tag color={asset.grow ? 'green' : 'red'}>{asset.growPercent}%</Tag>}
+                    {item.isPlain && item.value}
+                    {!item.isPlain && <Typography.Text type={asset.grow ? 'success' : 'danger'}>{item.value.toFixed(2)}$</Typography.Text>}
+                    </span>
                 </List.Item>
             )}
             />
             </Card>
-            <Card>
-                <Statistic
-                  title="Idle"
-                  value={9.3}
-                  precision={2}
-                  styles={{ content: { color: '#cf1322' } }}
-                  prefix={<ArrowDownOutlined />}
-                  suffix="%"
-                />
-            </Card>
+            ))}
         </Layout.Sider>
     )
 }
